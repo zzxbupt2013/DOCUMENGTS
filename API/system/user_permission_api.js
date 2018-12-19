1841f6c6ef6acb8cc68e5605f59e0bbeb8ec28c1
zzxbupt2013 <zhuzhenghang5211@163.com>
2018-12-19 14:41:54 +0800

    Update user_permission_api.js

--- API/system/user_permission_api.js
+++ API/system/user_permission_api.js
@@ -128,7 +128,7 @@ req = {
 }
 Sucess:
     res = {
-           biiboardData:[{
+           billboardData:[{
                 billboardId:'string',
                 connectCompany:'string', 
            }],
