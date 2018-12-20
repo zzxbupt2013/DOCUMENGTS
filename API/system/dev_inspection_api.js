809ddbcb37d90419245e63094b6abf81118f4ba7
zzxbupt2013 <zhuzhenghang5211@163.com>
2018-12-20 10:50:38 +0800

    Create dev_inspection_api.js

---
+++ API/system/dev_inspection_api.js
@@ -0,0 +1,43 @@
+/*
+*设备资产管理--资产巡检
+* */
+var req = {}   //请求数据
+var res = {}   //返回数据
+/*
+* 1、查询记录
+* 方法：POST
+* 路由：/system/dev_inspection/queryRecord
+* */
+req = {
+    userName: 'string',
+    queryData:{
+       recordType:'string',
+       startDate:'string',
+       endDate:'string',
+    }
+  
+}
+Sucess:
+    res = {
+           recordData:['记录1','记录2','记录3',...],
+    }
+Error:返回一个error
+/*
+* 1、新增记录
+* 方法：POST
+* 路由：/system/dev_inspection/addRecord
+* */
+req = {
+    userName: 'string',
+    addData:{
+       recordType:'string',
+       recordDate:'string',
+       recordDetail:'string',
+    }
+  
+}
+Sucess:
+    res = {
+           msg:'添加成功'
+    }
+Error:返回一个error
