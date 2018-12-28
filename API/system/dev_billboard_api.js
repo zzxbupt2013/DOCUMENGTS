b9f3899a377c4ac315c1dfd3e8fb48cda157d6e8
zzxbupt2013 <zhuzhenghang5211@163.com>
2018-12-28 16:21:40 +0800

    Update dev_billboard_api.js

--- API/system/dev_billboard_api.js
+++ API/system/dev_billboard_api.js
@@ -30,10 +30,27 @@ req = {
 }
 Sucess:
     res = {
-           billboardDetail:[{    //{label:'位置描述',value:'马朱路与京福路交口东南角'}
-               label:'string',
-               value:'string',
-           },],
+            billboardDetail: {                  //17项信息
+               billboard_dev_id:'string',//广告牌编号
+               billboard_position_msg:'string', //位置描述
+               billboard_gps_lon:'string',//经度
+       billboard_gps_lat:'string',//纬度
+       billboard_navigation:'string',//导航点
+       billboard_police:'string',//派出所
+       billboard_land:'string',//用地属性
+       billboard_layout:'string',//版面描述
+       billboard_power:'string', //供用电情况
+       billboard_light_type:'string',//灯具型号
+       billboard_power_route:'string', // 供电路由
+       billboard_canvas:'string',  //画面喷绘信息
+       billboard_base:'string',  //设施基础
+       billboard_height:'string',  //柱体高度
+       billboard_diameter:'string', //柱体直径
+       billboard_structure:'string',//设施结构
+       billboard_complete_date:'string',//竣工日期
+       billboard_remark:'string',//备注信息
+                billboard_sensor:'string'//关联传感器信息
+    }
     }
 Error:返回一个error
 /*
@@ -44,10 +61,27 @@ Error:返回一个error
 req = {
     userName: 'string', 
     billboardId:'string',
-    billboardDetail:[{    //{label:'位置描述',value:'马朱路与京福路交口东南角'}
-               label:'string',
-               value:'string',
-           },],
+    billboardDetail:{                  //17项信息
+               billboard_dev_id:'string',//广告牌编号
+               billboard_position_msg:'string', //位置描述
+               billboard_gps_lon:'string',//经度
+       billboard_gps_lat:'string',//纬度
+       billboard_navigation:'string',//导航点
+       billboard_police:'string',//派出所
+       billboard_land:'string',//用地属性
+       billboard_layout:'string',//版面描述
+       billboard_power:'string', //供用电情况
+       billboard_light_type:'string',//灯具型号
+       billboard_power_route:'string', // 供电路由
+       billboard_canvas:'string',  //画面喷绘信息
+       billboard_base:'string',  //设施基础
+       billboard_height:'string',  //柱体高度
+       billboard_diameter:'string', //柱体直径
+       billboard_structure:'string',//设施结构
+       billboard_complete_date:'string',//竣工日期
+       billboard_remark:'string',//备注信息
+               billboard_sensor:'string'//关联传感器信息
+    }
 }
 Sucess:
     res = {
